59332616e76388f465eea16823925bffdd515400
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-05-27 22:44:52 +0100

    create shader class to encapsulate shader creation and compilation logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include "shader.h"
 #include <iostream>
 
 const char* vertexShaderSource = "#version 330 core\n"
@@ -30,23 +30,27 @@ GLFWwindow* initWindow();
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-float vertices[] = {
-    // positions         // colors
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
-};
-
-// The order in which we draw vertices.
-unsigned int indices[] = { 
-    0, 1, 2,
-};
 
 int main()
 {
     GLFWwindow* window = initWindow();
 
-    unsigned int VBO, VAO, EBO, vertexShader, fragmentShader, shaderProgram;
+    Shader shader("shader.vs", "shader.fs");
+
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    };
+
+    // The order in which we draw vertices.
+    unsigned int indices[] = {
+        0, 1, 2,
+    };
+
+    unsigned int VBO, VAO, EBO;
+
     glGenBuffers(1, &VBO); // Assign an ID of 1 to the VBO.
 
     // Vertex Array Object set up.
@@ -61,40 +65,7 @@ int main()
     glGenBuffers(1, &EBO); // Generates Elements buffer object
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // Binds our buffer.
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Vertex Shader creation.
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    // Check that the vertex shader can compile...
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // Fragment Shader creation.
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    // Creating a Shader Program and linking our shaders to it.
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Ensure the shader program can correctly link the shaders.
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-    }
-
+ 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -102,11 +73,6 @@ int main()
     // color attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
-    // Delete old shader instances.
-    // Shaders are now part of the shader program, so these are no longer needed.
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
 
     // render loop
     while (!glfwWindowShouldClose(window))
@@ -118,15 +84,7 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-
-        // Get the amount of time (in seconds) the program has been running for.
-        // Calculate a colour based on this time.
-        // Get the newColor global uniform variable by it's name.
-        // Assign a value to newColor using the colour generated from greenValue.
-        // For each render cycle, a new colour will be generated and assigned to newColor.
-        // The shader program will use this new colour in the fragment shader every frame.
-        updateColorUniform(shaderProgram);
+        shader.use();
 
         // Bind a new VAO instance for rendering.
         glBindVertexArray(VAO);
